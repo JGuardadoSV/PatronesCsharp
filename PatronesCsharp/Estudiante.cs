67618641a4de6a35a50d3f4204cac9aed1da40f6
Josué Guardado <jguardadosv@outlook.com>
2021-05-21 09:11:51 -0600

    Agregar archivos de proyecto.

---
+++ PatronesCsharp/Estudiante.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronesCsharp
+{
+    public class Estudiante //SINGLETON
+    {
+        // Propiedades
+        public string Nombre { get; set; }
+        //***********************************
+        private static Estudiante instancia; //Instancia única
+        //Constructor por defecto privado
+        private Estudiante() {
+            this.Nombre = "50k registros";
+        } 
+        public static Estudiante ObtenerInstancia()
+        {
+            if (instancia==null) instancia = new Estudiante(); // ya no será nulo
+
+            return instancia;
+        }
+    }
+}
---
+++ PatronesCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronesCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Estudiante e1 = Estudiante.ObtenerInstancia();
+
+            Console.WriteLine(e1.Nombre);
+            
+
+
+           
+            
+        }
+    }
+}
